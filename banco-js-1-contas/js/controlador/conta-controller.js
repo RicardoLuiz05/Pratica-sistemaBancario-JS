be77919689f870c311754a58f2780bfd65aed3ff
Ricardo Luiz <ricardoluiz05.59@gmail.com>
2023-08-31 14:47:06 -0300

    inicio

---
+++ banco-js-1-contas/js/controlador/conta-controller.js
@@ -0,0 +1,57 @@
+class ContaController {
+    constructor() {
+        this.repositorioContas = new RepositorioContas();
+    }
+
+    adicionarConta(conta) {
+        this.repositorioContas.adicionar(conta);
+    }
+
+    listar() {
+        this.repositorioContas.getContas().forEach(conta =>
+            this.inserirContaNoHTML(conta)
+        );
+    }
+
+    inserir(evento) {
+        evento.preventDefault();
+        const elementoNumero = document.querySelector('#numero');
+        const elementoSaldo = document.querySelector('#saldo');
+        const elementoAniversario = document.querySelector('#dataAniversario');
+        const elementoTipo = document.querySelector('#contas')
+        
+        if (elementoTipo.value() == 'conta') {
+            const conta = new Conta(elementoNumero.value,
+                Number(elementoSaldo.value));
+            this.repositorioContas.adicionar(conta);
+            this.inserirContaNoHTML(conta);
+
+        }else if (elementoTipo.value() == 'conta-bonificada'){
+            const conta = new ContaBonificada(elementoNumero.value,
+                Number(elementoSaldo.value));
+            this.repositorioContas.adicionar(conta);
+            this.inserirContaNoHTML(conta);
+
+        }else {
+            const conta = new Polpanca(elementoNumero.value,
+                Number(elementoSaldo.value));
+            this.repositorioContas.adicionar(conta);
+            this.inserirContaNoHTML(conta);
+        }
+    }
+
+    inserirContaNoHTML(conta) {
+        const elementoP = document.createElement('p');
+        elementoP.textContent = 'Conta ' + conta.numero + ': ' + conta.saldo;
+        const botaoApagar = document.createElement('button');
+        botaoApagar.textContent = 'X';
+
+        botaoApagar.addEventListener('click', (event) => {
+            this.repositorioContas.remover(conta.numero);
+            event.target.parentElement.remove();
+        });
+
+        elementoP.appendChild(botaoApagar);
+        document.body.appendChild(elementoP);
+    }
+}
---
+++ banco-js-1-contas/js/modelo/polpanca.js
@@ -0,0 +1,8 @@
+class Polpanca extends Conta {
+    constructor(numero, saldo, dataAniversario){
+        super(numero, saldo);
+        this.dataAniversario = dataAniversario;
+        
+    }
+
+}
